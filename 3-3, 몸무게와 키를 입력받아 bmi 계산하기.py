979c18fa4ebccc7ca71aef7c9fd292e5720446a4
LCW3064 <113826357+LCW3064@users.noreply.github.com>
2022-12-09 23:46:51 +0900

    Update 3-3, 몸무게와 키를 입력받아 bmi 계산하기.py
    종료를 알리는 문 추가

--- 3-3, 몸무게와 키를 입력받아 bmi 계산하기.py
+++ 3-3, 몸무게와 키를 입력받아 bmi 계산하기.py
@@ -6,3 +6,4 @@ weight = float(input('몸무게를 kg 단위로 입력하시오: '))
 height = float(input('키를 미터 단위로 입력하시오: '))
 bmi = (weight / (height ** 2))# bmi 공식
 print('당신의 BMI=', bmi)# 결과값 출력
+print('프로그램을 종료합니다.')
