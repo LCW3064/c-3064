c1ef469da88939ab90397e6b8bdb524cc0949552
LCW3064 <113826357+LCW3064@users.noreply.github.com>
2022-12-10 02:22:02 +0900

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include "CoffeeMachine.h"
+
+int main() {
+	CoffeeMachine java(5, 10);
+	cout << "커피 머신 상태\n";
+	cout << "커피량: 5, 물량 : 10으로 초기화\n\n";
+	java.drinkEspresso();
+	java.show();
+	java.drinkAmericano();
+	java.show();
+	java.fill();
+	java.show();
+}
