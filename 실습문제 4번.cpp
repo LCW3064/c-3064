7a30c4f47647c449d59f12caed96b28ca8a7a181
LCW3064 <113826357+LCW3064@users.noreply.github.com>
2022-12-10 01:56:53 +0900

    Add files via upload

---
+++ 실습문제 4번.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+class Buffer {
+	string text;
+public:
+	Buffer(string text) { this->text = text; }
+	void add(string next) { text += next; } // text에 next 문자열 덧붙이기 
+	void print() { cout << text << endl; }
+};
+
+Buffer& append(Buffer& a, string s) {
+	a.add(s);
+	return a;
+}
+
+int main() {
+	Buffer buf("Hello");
+	Buffer& temp = append(buf, "Guys"); // buf의 문자열에 "Guys" 덧붙임 
+	temp.print(); // "HelloGuys" 출력
+	buf.print(); // "HelloGuys" 출력
+}
