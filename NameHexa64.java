a2ade08ac1a5a5df33edf7260b296ab7346167ac
LCW3064 <113826357+LCW3064@users.noreply.github.com>
2022-12-10 02:24:54 +0900

    Add files via upload

---
+++ NameHexa64.java
@@ -0,0 +1,12 @@
+
+public class NameHexa64 {
+	public static void main(String[] args) {
+		char a = '이'; 
+		char b = '창'; 
+		char c = '우';
+		
+		System.out.println("이 : " + Integer.toHexString(a) + " 창 : " + 
+		Integer.toHexString(b) + " 우 : " + Integer.toHexString(b));
+		
+	}
+}
