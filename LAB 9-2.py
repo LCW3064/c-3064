c6358d973deaa59c433f256d4dc9d15133dcd9dd
LCW3064 <113826357+LCW3064@users.noreply.github.com>
2022-12-10 01:09:56 +0900

    Update LAB 9-2.py
    종료를 알리는 문 추가

--- LAB 9-2.py
+++ LAB 9-2.py
@@ -7,3 +7,4 @@ t = "There's a reason some people are working to make it harder to vote, \
 especially for people of color. It's because when we show up, things change."
 length = len(t.split(' '))
 print("word count: ",length)
+print('프로그램을 종료합니다.')
