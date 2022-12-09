550f9c564639adde797ed4bb2b075c250f40fe98
LCW3064 <113826357+LCW3064@users.noreply.github.com>
2022-12-10 02:08:54 +0900

    Add files via upload

---
+++ 8실습 1번.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Circle {
+	int radius;
+public:
+	Circle(int radius = 0) { this->radius = radius; }
+	int getRadius() { return radius; }
+	void setRadius(int radius) { this->radius = radius; }
+	double getArea() { return 3.14 * radius * radius; }
+};
+
+class NamedCircle : public Circle {
+	string name;
+public:
+	NamedCircle(int r, string n) { setRadius(r); this->name = n; }
+	void setName(string s) { this->name = s; }
+	string getName() { return this->name; }
+	void show();
+};
+
+void NamedCircle::show() {
+	cout << "반지름이 " << getRadius() << "인 " << getName() << endl;
+}
+
+int main() {
+	NamedCircle waffle(3, "waffle");
+	waffle.show();
+}
