b38e5ae6a46db31bc66f43ea8cf89b2d9618335e
LCW3064 <113826357+LCW3064@users.noreply.github.com>
2022-12-10 01:55:46 +0900

    Add files via upload

--- 실습문제 1번.cpp
+++ 실습문제 1번.cpp
@@ -1,14 +1,35 @@
 #include <iostream>
 using namespace std;
 
+class Circle;
+void swap(Circle& c, Circle& cc);
+
+class Circle {
+	int r;
+public:
+	Circle(int r = 1) { this->r = r; }
+	void setRadius(int r) { this->r = r; }
+	int getRadius() { return r; }
+	double Area() { return r * r * 3.14; }
+};
+
+void swap(Circle& c, Circle& cc) {
+	int n;
+
+	n = c.getRadius();
+	c.setRadius(cc.getRadius());
+	cc.setRadius(n);
+}
+
 int main() {
-	int* p = new int[5];
-	double avg = 0;
-	cout << "정수 5개 입력 >>";
-	for (int i = 0; i < 5; i++) {
-		cin >> p[i];
-		avg += p[i];
-	}
-	cout << "평균" << (avg / 5);
-	delete[]p;
+	Circle c1(5);
+	Circle c2(10);
+
+	cout << "c1 반지름 = " << c1.getRadius() << " 넓이 = " << c1.Area() << endl;
+	cout << "c2 반지름 = " << c2.getRadius() << " 넓이 = " << c2.Area() << endl;
+	cout << endl <<"<swap 함수 호출 후>" << endl;
+	swap(c1, c2);
+	cout << "c1 반지름 = " << c1.getRadius() << " 넓이 = " << c1.Area() << endl;
+	cout << "c2 반지름 = " << c2.getRadius() << " 넓이 = " << c2.Area() << endl;
+
 }
