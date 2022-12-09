427ac178eaf5435d90e973cbf210b713e2bd5cc7
LCW3064 <113826357+LCW3064@users.noreply.github.com>
2022-12-10 01:56:29 +0900

    Add files via upload

--- 실습문제 3번.cpp
+++ 실습문제 3번.cpp
@@ -1,23 +1,29 @@
-#include <iostream>
-#include <string>
+#include<iostream>
 using namespace std;
 
+class Accumulator {
+	int value;
+public:
+	Accumulator(int value); // 매개 변수 value로 멤버 value를 초기화한다. 
+	Accumulator& add(int n); // value에 n을 더해 값을 누적한다. 
+	int get(); // 누적된 값 value를 리턴한다. 
+};
+
+Accumulator::Accumulator(int value) {
+	this->value = value;
+}
+
+Accumulator& Accumulator::add(int n) {
+	value = value + n;
+	return *this;
+}
+
+int Accumulator::get() {
+	return value;
+}
+
 int main() {
-
-	cout << "문자열 입력>> ";
-	string str;
-	getline(cin, str, '\n');
-
-	int n = 0;
-	int l = 0;
-	while (true) {
-		l = str.find('a', l + 1);
-		if (l == -1) {
-			break;
-		}
-		else
-			n++;
-	}
-
-	cout << "문자 a는 " << n << "개 있습니다.";
+	Accumulator acc(10);
+	acc.add(5).add(6).add(7); // acc의 value 멤버가 28이 된다. 
+	cout << acc.get(); // 28 출력 
 }
