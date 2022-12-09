3f904edc80208e83290bfd703d609d748af3461f
LCW3064 <113826357+LCW3064@users.noreply.github.com>
2022-12-10 02:32:00 +0900

    Add files via upload

---
+++ 예제 10-1.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+char str[] = { '*','*','W','e','l','c','o','m','e','*','*' };
+void main()
+{
+	printf("%c%c%c%c%c%c%c%c%c%c%c\n",str[0], str[1], str[2], str[3], str[4], str[5], str[6], str[7], str[8], str[9], str[10]);
+}
